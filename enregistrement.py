fc833ce0359ff5fa0ffa554c66efd41a5ba9b742
Lidgeno <67106684+Lidgeno@users.noreply.github.com>
2020-06-19 17:38:33 +0200

    Update enregistrement.py

--- enregistrement.py
+++ enregistrement.py
@@ -1,24 +1,4 @@
 import mysql.connector
-<<<<<<< Updated upstream
-mysql=mysql.connector.connect(host="localhost", user="root", passwd="", database="essai")
-
-
-#toute la table
-# Create Table
-mycursor.execute('''CREATE TABLE creation(id int, figure_1 VARCHAR(20), figure_2 VARCHAR(20), figure_3 VARCHAR(20), figure_4 VARCHAR(20), figure_5 VARCHAR(20))''')
-
-sql="insert into creation(id, figure_1, figure_2, figure_3, figure_4, figure_5) VALUES (%s,%s,%s,%s,%s,%s)"
-for i in range (1,nbfigures):
-    val=[
-        (liste_figures[i])
-        ]
-    mycursor.executemany(sql,val)
-
-
-mysql.commit();
-
-print("la table creation a ete completee");
-=======
 
 nbmaxfigures=5
 mysql=mysql.connector.connect(host="localhost", user="root", passwd="", database="genese")
@@ -34,4 +14,3 @@ def enregistrement(liste_figures):
     mysql.commit();
 
     print("la table creation a ete completee");
->>>>>>> Stashed changes
